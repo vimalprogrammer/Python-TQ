6a73066d5cbb55e37f6f53828230b3a8c971d73e
Vimal M <msvimal10@gmail.com>
2021-12-25 17:37:56 +0530

    set the zeros to the end

---
+++ ZerosAtEnd.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+// ar={1,5,0,0,4,5,0,9}
+// ar={1,5,4,5,9,0,0,0}
+
+//loop ? 0: do swap with the next non zero number
+
+int main()
+{
+    //cout<<"Hello World";
+    int n;cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+        cin>>a[i];
+    for(int i=0;i<n;i++)
+    {
+        if(a[i]==0)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                if(a[j]!=0)
+                {
+                    int temp=a[i];
+                    a[i]=a[j];
+                    a[j]=temp;
+                    break;
+                }
+            }
+        }
+    }
+    for(int i=0;i<n;i++)
+        cout<<a[i]<<" ";    
+
+    return 0;
+}

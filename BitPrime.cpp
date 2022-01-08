041ed0dbea59af596d092153bbdd7af3bc2567c6
vimalprogrammer <msvimal10@gmail.com>
2022-01-08 14:58:12 +0530

    Bit String Prime checking

---
+++ BitPrime.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+//Code here...
+    int t;cin>>t;
+    while(t--)
+    {
+	    string s;cin>>s;
+	    int n=s.length();
+	    if(n==1)
+	    {	
+	    	cout<<"No\n";
+	    	continue;
+	    }
+	    int cnt=0,pos=-1;
+	    for(int i=0;i<n;i++)
+	    {
+	    	if(s[i]=='1')
+	    	{
+	    		cnt++;
+	    		pos=i+1;
+	    	}
+	    }
+	   	if(cnt==0 || (cnt==1 && pos==n))
+	   		cout<<"No\n";
+	   	else
+	   		cout<<"Yes\n";
+
+	}
+}

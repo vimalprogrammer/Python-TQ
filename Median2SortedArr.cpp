17e93a7f16d2a14e54388bd4f2b30e75c099274c
vimalprogrammer <msvimal10@gmail.com>
2022-01-22 13:45:27 +0530

    Bsearch - Median of two sorted arrays

---
+++ Median2SortedArr.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) 
+{
+        if(nums1.size()>nums2.size()){
+            return findMedianSortedArrays(nums2,nums1);
+        }
+        int x = nums1.size();
+        int y = nums2.size();
+        
+        int high = x;
+        int low = 0;
+        while(low<=high){
+            int divideX = low + (high - low)/2;
+            int divideY = (x + y + 1)/2 - divideX;
+            
+            double maxLeftX = (divideX == 0 ? INT_MIN : nums1[divideX-1]);
+            double minRightX = (divideX == x ? INT_MAX : nums1[divideX]);
+            
+            double maxLeftY = (divideY == 0 ? INT_MIN : nums2[divideY-1]);
+            double minRightY = (divideY == y ? INT_MAX : nums2[divideY]);
+            
+            if(maxLeftX <= minRightY && maxLeftY <= minRightX){
+                if((x+y)%2==0){
+                    return (max(maxLeftX,maxLeftY) + min(minRightX,minRightY))/2.0;
+                }else{
+                    return max(maxLeftX,maxLeftY);
+                }
+            }else if(maxLeftX>minRightY){
+                high = divideX-1;
+            }else{
+                low = divideX +1;
+            }
+        }
+        return 0;
+    }
+
+int main()
+{
+    //cout<<"Hello World";
+    vector<int>v1={1,3};
+    vector<int>v2={2};
+    double res=findMedianSortedArrays(v1,v2);
+    cout<<res;
+    return 0;
+}

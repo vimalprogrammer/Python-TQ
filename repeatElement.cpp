1b391e1baef5e2262777facec4e7637915fd2967
vimalprogrammer <msvimal10@gmail.com>
2022-01-24 19:31:58 +0530

    with space - repeating element inclusive of range

--- repeatElement.cpp
+++ repeatElement.cpp
@@ -19,3 +19,30 @@ public:
     return slow;
     }
 };
+
+
+//With auxillary space 
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+//Code here...
+    vector<int>v={5,7,0,1,3,2,4,1};
+    int n=v.size();
+    vector<int>temp(n);
+    for(int i=0;i<n;i++)
+    {
+    	temp[v[i]]++;
+    	if(temp[v[i]]>1)
+    	{
+    		cout<<v[i];
+    		break;
+    	}
+    }
+
+    return 0;
+}
+*/

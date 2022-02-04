670377730655737468468f1baf9fa3c8c354649e
vimalprogrammer <msvimal10@gmail.com>
2022-02-04 13:57:59 +0530

    find anagrams solution practise

--- findAnagrams.cpp
+++ findAnagrams.cpp
@@ -1,18 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> findAnagrams(string s, string p) {
+vector<int> findAnagrams(string s, string p)
+{
         int n = s.size(),m=p.size();
         vector<int> ans;
         if(m>n)return ans;
         vector<int> a(26,0),b(26,0);
-        for(auto x: p){
+        for(auto x: p)
             a[x-'a']++;
-        }
-        for(int i=0;i<m;i++){
+
+        for(int i=0;i<m;i++)
             b[s[i]-'a']++;
-        }
-        for(int i=m;i<n;i++){
+
+        for(int i=m;i<n;i++)
+        {
             if(a==b)ans.push_back(i-m);
             b[s[i-m]-'a']--;
             b[s[i]-'a']++;
@@ -29,3 +31,42 @@ for(auto it:v)
     cout<<it<<" ";
     return 0;
 }
+
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> findAnagrams(string s, string p) 
+{
+    vector<int>res;
+    int m=s.length();
+    int n=p.length();
+    if(n>m)
+        return {};
+    vector<int>dummy1(26,0);
+    vector<int>dummy2(26,0);
+    for(int i=0;i<n;i++)
+        dummy1[p[i]-'a']++;//pattern array
+    for(int i=0;i<n;i++)
+        dummy2[s[i]-'a']++; 
+    for(int i=n;i<m;i++)
+    {
+        if(dummy1==dummy2)
+            res.push_back(i-n);
+        dummy2[s[i-n]-'a']--;
+        dummy2[s[i]-'a']++;
+    }
+    if(dummy1==dummy2)
+        res.push_back(m-n);//for checking the last anagram  
+    return res;
+}
+
+int main()
+{
+//    cout<<"Hello World";
+vector<int>v=findAnagrams("abab","ab");
+for(auto it:v)
+    cout<<it<<" ";
+    return 0;
+}
+*/

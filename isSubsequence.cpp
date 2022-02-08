acc6f3bd7d6218ee3baabbc4c8b9a0287b670c73
vimalprogrammer <msvimal10@gmail.com>
2022-02-08 13:12:56 +0530

    recursive approach added for isSubseq problem

--- isSubsequence.cpp
+++ isSubsequence.cpp
@@ -52,3 +52,35 @@ bool isSubsequence(string s, string t)
         return false;
 }
 */
+
+//RECURSIVE APPROACH
+#include <bits/stdc++.h>
+using namespace std;
+
+bool issubsq(string s,string t,int n,int m)
+{
+    if(n==0)
+        return true;
+    if(m==0)
+        return false;
+    if(s[n-1]==t[m-1])
+        return issubsq(s,t,n-1,m-1);
+    else
+        return issubsq(s,t,n,m-1);
+}
+
+bool isSubsequence(string s, string t) 
+{
+    int n=s.length();
+    int m=t.length();
+    bool res=issubsq(s,t,n,m);
+    return res;
+}
+
+int main()
+{
+    //cout<<"Hello World";
+    string s="abc",t="ahbjcj";
+    cout<<isSubsequence(s,t);
+    return 0;
+}

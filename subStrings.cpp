2f228e47e853fa2c4db20a2f10a82184feacc71e
vimalprogrammer <msvimal10@gmail.com>
2022-02-12 19:50:07 +0530

    without classe - generate all subsets of a string added

--- subStrings.cpp
+++ subStrings.cpp
@@ -36,3 +36,35 @@ int main()
     	cout<<it<<" ";
     return 0;
 }
+
+
+//without classes and its access modifiers
+/*
+#include <bits/stdc++.h>
+using namespace std;
+
+
+vector<string>v;
+
+vector<string> subStrings(string s,string curSubStr,int i=0)
+{
+    if(i==s.length())
+    	v.push_back(curSubStr);
+	else
+	{
+		subStrings(s,curSubStr+s[i],i+1);
+		subStrings(s,curSubStr,i+1);
+	}
+	return v;
+}
+
+int main()
+{
+    //cout<<"Hello World";
+    string s="abc";
+    vector<string>res=subStrings(s,"",0);
+    for(auto it:res)
+    	cout<<it<<" ";
+    return 0;
+}
+*/

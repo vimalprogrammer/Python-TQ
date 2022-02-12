1c9e21d7deba7f36bf205c4fd2c5c644bec58399
vimalprogrammer <msvimal10@gmail.com>
2022-02-12 13:21:48 +0530

    add until it becomes a single digit

---
+++ addTillSingle.cpp
@@ -0,0 +1,35 @@
+/*
+Given an integer num, repeatedly add all its digits 
+until the result has only one digit, and return it.
+
+Example 1:
+
+Input: num = 38
+Output: 2
+Explanation: The process is
+38 --> 3 + 8 --> 11
+11 --> 1 + 1 --> 2 
+Since 2 has only one digit, return it.
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int addDigits(int num) 
+    {
+        int sum=0;
+        while(num>9)
+        {
+            while(num)
+            {
+                sum+=(num%10);
+                num/=10;
+            }
+            num=sum;
+            sum=0;
+        }
+        return num;
+    }   
+};

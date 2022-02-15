01ceda6f1636e057326b76ba98c58dc5c5ee1ca0
vimalprogrammer <msvimal10@gmail.com>
2022-02-15 07:51:07 +0530

    single digit 2 - leetcode medium

---
+++ singleDigit2.cpp
@@ -0,0 +1,44 @@
+/*
+Given an integer array nums where every element appears three times except for one, which appears exactly once. Find the single element and return it.
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+Example 1:
+Input: nums = [2,2,3,2]
+Output: 3
+
+Example 2:
+Input: nums = [0,1,0,1,0,1,99]
+Output: 99
+*/
+
+
+//O(n^2) solution
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) 
+    {
+        for ( int i = 0; i < nums.size(); i++) 
+        {
+            int flag=0;
+            for ( int j = 0; j < nums.size(); j++)
+                if ( nums[i] == nums[j] && i != j)
+                    flag=1;
+                    
+            if (flag==0) 
+                return nums[i];
+        }
+        return -1;
+    }
+};
+
+int main()
+{
+    //cout<<"Hello World";
+    Solution s;
+    vector<int>v={2,2,7,2};
+    cout<<s.singleNumber(v);
+    return 0;
+}

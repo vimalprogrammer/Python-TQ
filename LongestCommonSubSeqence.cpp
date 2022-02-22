ca94d68ad80b5b5197edd428b6f08cefd614a47b
vimalprogrammer <msvimal10@gmail.com>
2022-02-22 19:34:01 +0530

    leetcode - medium | Memoization | AC

--- LongestCommonSubSeqence.cpp
+++ LongestCommonSubSeqence.cpp
@@ -50,3 +50,32 @@ public:
     }
 };
 
+//-----------------ACCEPTED MEMOIZATION SOLUTION-----------------//
+
+class Solution {
+public:
+    int longestCommonSubsequence(string &s1,string &s2,int n1,int n2,int memo[1001][1001])
+    {
+        if(n1==0 || n2==0)
+            return 0;
+        if(memo[n1][n2]!=-1)
+            return memo[n1][n2];
+        if(s1[n1-1]==s2[n2-1])
+            return memo[n1][n2]=1+longestCommonSubsequence(s1,s2,n1-1,n2-1,memo);
+        else
+            return memo[n1][n2]=max(longestCommonSubsequence(s1,s2,n1-1,n2,memo),longestCommonSubsequence(s1,s2,n1,n2-1,memo));
+    }
+    
+    int longestCommonSubsequence(string text1, string text2) {
+     
+        int n1=text1.size();
+        int n2=text2.size();
+        int memo[1001][1001];
+        for(int i=0;i<1001;i++)
+            for(int j=0;j<1001;j++)
+                memo[i][j]=-1;
+        
+        return longestCommonSubsequence(text1,text2,n1,n2,memo);
+    }
+};
+

090f90f6ab23dff38b8e09a26df8c45886eee5ad
vimalprogrammer <msvimal10@gmail.com>
2022-03-01 21:31:14 +0530

    Leetcode - easy || Tribonacci || DP-Tabulation

--- Nth-Tribonacci.cpp
+++ Nth-Tribonacci.cpp
@@ -48,3 +48,16 @@ int main()
     }
     return 0;
 }
+
+//Tabulation - DP
+
+    int tribonacci(int n) {
+
+        int arr[39];
+        arr[0]=0; arr[1]=1;arr[2]=1;
+        if(n<=2) return arr[n];
+        for(int i=3;i<=n;i++){
+            arr[i]=arr[i-1]+arr[i-2]+arr[i-3];
+        }
+        return arr[n];
+    }

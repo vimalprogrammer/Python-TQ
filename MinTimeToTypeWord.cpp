1bfa0c7f23700f602f4426fc90a876ae028280e0
vimalprogrammer <msvimal10@gmail.com>
2022-03-04 21:34:03 +0530

    Minimum Time to Type Word Using Special Typewriter

---
+++ MinTimeToTypeWord.cpp
@@ -0,0 +1,72 @@
+/*
+There is a special typewriter with lowercase English letters 'a' to 'z' arranged in a circle with a pointer. A character can only be typed if the pointer is pointing to that character. The pointer is initially pointing to the character 'a'.
+Each second, you may perform one of the following operations:
+
+Move the pointer one character counterclockwise or clockwise.
+Type the character the pointer is currently on.
+Given a string word, return the minimum number of seconds to type out the characters in word. 
+
+Example 1:
+Input: word = "abc"
+Output: 5
+
+Explanation: 
+The characters are printed as follows:
+- Type the character 'a' in 1 second since the pointer is initially on 'a'.
+- Move the pointer clockwise to 'b' in 1 second.
+- Type the character 'b' in 1 second.
+- Move the pointer clockwise to 'c' in 1 second.
+- Type the character 'c' in 1 second.
+
+Example 2:
+Input: word = "bza"
+Output: 7
+Explanation:
+The characters are printed as follows:
+- Move the pointer clockwise to 'b' in 1 second.
+- Type the character 'b' in 1 second.
+- Move the pointer counterclockwise to 'z' in 2 seconds.
+- Type the character 'z' in 1 second.
+- Move the pointer clockwise to 'a' in 1 second.
+- Type the character 'a' in 1 second.
+
+Example 3:
+Input: word = "zjpc"
+Output: 34
+Explanation:
+The characters are printed as follows:
+- Move the pointer counterclockwise to 'z' in 1 second.
+- Type the character 'z' in 1 second.
+- Move the pointer clockwise to 'j' in 10 seconds.
+- Type the character 'j' in 1 second.
+- Move the pointer clockwise to 'p' in 6 seconds.
+- Type the character 'p' in 1 second.
+- Move the pointer counterclockwise to 'c' in 13 seconds.
+- Type the character 'c' in 1 second.
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int minTimeToType(string word) 
+{
+    char prevChar = 'a';
+    int totalTime = word.length();
+    for(int i = 0; i < word.length(); i++)
+    {
+        char currChar = word[i];
+        int diff = abs(currChar - prevChar);
+        totalTime += min(diff, 26 - diff);
+        prevChar = currChar;
+    }
+    return totalTime;
+}
+
+int main()
+{
+//    cout<<"Hello World";
+    cout<<minTimeToType("abc");
+    return 0;
+}
+

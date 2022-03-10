889a43c77abb727e583a3ac017e640285da70955
vimalprogrammer <msvimal10@gmail.com>
2022-03-10 23:17:51 +0530

    Leetcode easy

---
+++ CountItemsMatchingARule.cpp
@@ -0,0 +1,42 @@
+/*
+You are given an array items, where each items[i] = [typei, colori, namei] describes the type, color, and name of the ith item. You are also given a rule represented by two strings, ruleKey and ruleValue.
+
+The ith item is said to match the rule if one of the following is true:
+
+ruleKey == "type" and ruleValue == typei.
+ruleKey == "color" and ruleValue == colori.
+ruleKey == "name" and ruleValue == namei.
+Return the number of items that match the given rule.
+
+Example 1:
+
+Input: items = [["phone","blue","pixel"],["computer","silver","lenovo"],["phone","gold","iphone"]], ruleKey = "color", ruleValue = "silver"
+Output: 1
+Explanation: There is only one item matching the given rule, which is ["computer","silver","lenovo"].
+
+Example 2:
+
+Input: items = [["phone","blue","pixel"],["computer","silver","phone"],["phone","gold","iphone"]], ruleKey = "type", ruleValue = "phone"
+Output: 2
+Explanation: There are only two items matching the given rule, which are ["phone","blue","pixel"] and ["phone","gold","iphone"]. Note that the item ["computer","silver","phone"] does not match.
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int countMatches(vector<vector<string>>& items, string ruleKey, string ruleValue) {
+        int i;
+        if(ruleKey=="type")i=0;
+        if(ruleKey=="color")i=1;
+        if(ruleKey=="name")i=2;
+        
+        int ans=0;
+        for(int j=0;j<items.size();j++){
+            if(items[j][i]==ruleValue)ans++;
+        }
+        
+        return ans;
+    }   
+};

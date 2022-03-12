53a27c4a85779bd6da76a6a233a310eaaf86965c
vimalprogrammer <msvimal10@gmail.com>
2022-03-12 21:11:56 +0530

    I/O.txt added

--- Rotate90degAntiClk.cpp
+++ Rotate90degAntiClk.cpp
@@ -33,11 +33,6 @@ void matrixRotAntiClkwise(vector<vector<int>>&v,int n)
 
 int main()
 {
-    #ifndef ONLINE_JUDGE
-        freopen("input.txt", "r", stdin);
-        freopen("output.txt", "w", stdout);
-    #endif
-
 //Code here...
     vector<vector<int>>v;
     int n;cin>>n;

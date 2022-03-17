9e6ba1c6ce3268433002b150b000ff0476772337
vimalprogrammer <msvimal10@gmail.com>
2022-03-17 23:48:34 +0530

    problem stmt added

---
+++ ReverseSumPalindrome.cpp
@@ -0,0 +1,6 @@
+/*
+Given a number 'n'. Reverse it and add the reverse number to 
+the original number, if the sum is not a pallindrome then repeat 
+the process until the number becomes a pallindrome.
+*/
+

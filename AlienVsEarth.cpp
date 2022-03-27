f5154bda7b5a6f7a231fe6ccc8e0c6978eb94d94
vimalprogrammer <msvimal10@gmail.com>
2022-03-27 09:28:51 +0530

    AC-World Army vs Aliens - Code Gladiator

--- AlienVsEarth.cpp
+++ AlienVsEarth.cpp
@@ -74,3 +74,30 @@ int main(int argc, char *a[])
     // }
 // 	cout<<hht<<" "<<mmt;
 }
+
+//---------------------------------------------------------------------//
+//AC Solution
+
+#include <stdio.h>
+
+int main() {
+
+    int departureTimeHH, departureTimeMM, TravelTimeHH, TravelTimeMM;
+    scanf("%d %d", &departureTimeHH, &departureTimeMM);
+    scanf("%d %d", &TravelTimeHH, &TravelTimeMM);
+    
+    int totalMinutesOfTravel = (TravelTimeHH * 60) + TravelTimeMM;
+    
+    for(int i=1; i<=totalMinutesOfTravel; i++){
+        departureTimeMM += 1;
+        if (departureTimeMM > 59){
+            departureTimeMM = 0;
+            departureTimeHH += 1;
+            if (departureTimeHH > 23){
+                departureTimeHH = 0;
+            }
+        }    
+    }
+    printf("%02d %02d", departureTimeHH, departureTimeMM);
+    return 0;
+}

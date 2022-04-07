17fd418d851153be07b2a8143167749f52ea5384
vimalprogrammer <msvimal10@gmail.com>
2022-04-07 21:57:17 +0530

    leetcode easy - Last Stone Weight

--- LastStoneEntry.cpp
+++ LastStoneEntry.cpp
@@ -24,3 +24,32 @@ Example 2:
 Input: stones = [1]
 Output: 1
 */
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& s) 
+    {
+         make_heap(s.begin(), s.end());
+        
+         while (s.size() > 1) {
+            int x = s.front();
+            pop_heap(s.begin(), s.end());
+            s.pop_back();
+            
+            int y = s.front();
+            pop_heap(s.begin(), s.end());
+            s.pop_back();
+            
+            if (x == y) continue;
+            
+            s.push_back(abs(x - y));
+            push_heap(s.begin(), s.end());
+        }
+        
+        return s.size() ? s.front() : 0;
+    }
+};

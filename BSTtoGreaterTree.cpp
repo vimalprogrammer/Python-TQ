323374995f622b1abce0f79be9491a71488baed1
vimalprogrammer <msvimal10@gmail.com>
2022-04-16 21:38:18 +0530

    leetcode med - Convert BST to Greater Tree

---
+++ BSTtoGreaterTree.cpp
@@ -0,0 +1,56 @@
+/*
+Given the root of a Binary Search Tree (BST), convert it to a Greater Tree such that every key of the original BST is changed to the original key plus the sum of all keys greater than the original key in BST.
+
+As a reminder, a binary search tree is a tree that satisfies these constraints:
+
+The left subtree of a node contains only nodes with keys less than the node's key.
+The right subtree of a node contains only nodes with keys greater than the node's key.
+Both the left and right subtrees must also be binary search trees.
+
+Example 1:
+Input: root = [4,1,6,0,2,5,7,null,null,null,3,null,null,null,8]
+Output: [30,36,21,36,35,26,15,null,null,null,33,null,null,null,8]
+
+Example 2:
+Input: root = [0,null,1]
+Output: [1,null,1]
+*/
+
+//--------------Solution---------------//
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    int curr_sum;
+    void traverse(TreeNode* node)
+    {
+        if(node->right)
+            traverse(node->right);
+        
+        curr_sum+=node->val;
+        node->val=curr_sum;
+        
+        if(node->left)
+            traverse(node->left);
+
+    }
+    
+    TreeNode* convertBST(TreeNode* root) {
+        if(!root)
+            return root;
+        
+        curr_sum=0;
+        traverse(root);
+        return root;
+    }
+};

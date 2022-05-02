a892a8da61041201ff974b673bf9c988dfc7fbeb
vimalprogrammer <msvimal10@gmail.com>
2022-05-02 22:20:20 +0530

    905. Sort Array By Parity

--- SortByParity.cpp
+++ SortByParity.cpp
@@ -35,3 +35,14 @@ public:
     return temp;
 }
 };
+
+//---------------- 3 Line Solution -------------------------
+
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+        for (int i = 0, j = 0; j < nums.size(); j++)
+            if (nums[j] % 2 == 0) swap(nums[i++], nums[j]);
+        return nums;
+    }
+};

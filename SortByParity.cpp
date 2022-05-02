55273b677be44e7bc71c44d14cc41e647eb87dee
vimalprogrammer <msvimal10@gmail.com>
2022-05-02 22:17:10 +0530

    905. Sort Array By Parity

---
+++ SortByParity.cpp
@@ -0,0 +1,37 @@
+/*
+Given an integer array nums, move all the even integers at the beginning of the array followed by all the odd integers.
+
+Return any array that satisfies this condition.
+
+Example 1:
+
+Input: nums = [3,1,2,4]
+Output: [2,4,3,1]
+Explanation: The outputs [4,2,3,1], [2,4,1,3], and [4,2,1,3] would also be accepted.
+Example 2:
+
+Input: nums = [0]
+Output: [0]
+*/
+
+class Solution {
+public:
+   vector<int> sortArrayByParity(vector<int>& nums) 
+{
+    int n=nums.size();
+    int k=0;
+    vector<int>res;
+    vector<int>temp(n);
+    for(int i=0;i<n;i++)
+    {
+        if(nums[i]%2==0)
+            temp[k++]=nums[i];
+        else
+            res.push_back(nums[i]);
+    }
+    n-=k;
+    for(int i=0;i<n;i++)
+        temp[k++]=res[i];
+    return temp;
+}
+};
